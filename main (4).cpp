9b46f144a14e9020ed88a47a9fab53d3a918d0cf
sadaf7 <78465817+sadaf7@users.noreply.github.com>
2021-02-22 11:11:03 -0800

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Size of Data Types " << endl;
+    cout << "The size of char is : " << sizeof(char) << " bytes" << endl;
+    cout << "Ther size of int is : " << sizeof(int) << " bytes" << endl;
+    cout << "The size of double is : " << sizeof(double) << " bytes" << endl;
+    cout << "The size of long double is : " << sizeof(long double) << " bytes" << endl;
+    cout << "The size of string is : " << sizeof(string) << " bytes" << endl;
+    cout << "The size of bool is : " << sizeof(bool) << " bytes" << endl;
+    cout << "The size of float is : " << sizeof(float) << " bytes" << endl;
+    return 0;
+}
